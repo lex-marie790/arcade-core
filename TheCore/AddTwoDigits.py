45f76389714470ddf0af802fe3562b13a5dcac6a
lex-marie790 <lexi.marie790@gmail.com>
2021-01-17 15:44:07 -0800

    first commit

---
+++ TheCore/AddTwoDigits.py
@@ -0,0 +1,23 @@
+# You are given a two-digit integer n. Return the sum of its digits.
+
+# Example
+# For n = 29, the output should be
+# addTwoDigits(n) = 11.
+
+# Input/Output
+# [execution time limit] 4 seconds (py3)
+# [input] integer n
+# A positive two-digit integer.
+# Guaranteed constraints:
+# 10 ≤ n ≤ 99.
+# [output] integer
+# The sum of the first and second digits of the input number.
+
+def addTwoDigits(n):
+    n = list(str(n))
+    count = 0
+    for i in n:
+        count += int(i)
+    return count
+        
+print(addTwoDigits(29))
